b0a4469cd2a3b717ab3ac9f53ee7a8b704b7a46b
Andrew Horbach <andrewhorbach@gmail.com>
2021-08-14 14:39:33 +0300

    feat: heroku deployment

--- main.py
+++ main.py
@@ -18,11 +18,13 @@ async def root():
             "timestamp": datetime.now(),
             "uuid": uuid4()}
 
-@app.get("/isalive")
+
+@app.get("/isalive", status_code=status.HTTP_200_OK)
 async def liveness_probe():
     """Smoke test ep"""
     return {"status": "OK"}
 
-if __name__ == "__main__":
-    port = os.environ.get('PORT', 8080)
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
+# if __name__ == "__main__":
+#     port = os.environ.get('PORT', 8080)
+#     uvicorn.run(app, host="0.0.0.0", port=port)
